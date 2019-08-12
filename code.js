5eaf371e4633089569c972fdc98a6fe3da5cafdf
littlemiaooow <aiden_583@163.com>
2019-08-12 19:26:18 +0800

    111

---
+++ code.js
@@ -0,0 +1,8 @@
+runBehavior = {
+  //拥有execute方法的js对象
+  execute: function(sprite, now, fps, context, lastAnimationFrameTime) {
+    //更新sprite对象的属性
+  }
+};
+
+var runner = new Sprite("runner"); //...
